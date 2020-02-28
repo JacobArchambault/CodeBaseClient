80228de6b33a135302d2ac10df6b653b9d23b4b5
Jacob Archambault <jacob.archambault@waystar.com>
2020-02-27 23:46:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using CarLibrary;
+using System;
+
+namespace CodeBaseClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Shared Assembly Client *****");
+            _ = new SportsCar();
+            Console.WriteLine("Sports car has been allocated.");
+            Console.ReadLine();
+        }
+    }
+}
